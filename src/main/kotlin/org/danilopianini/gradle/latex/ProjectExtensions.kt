fe1a5a6ee11755b4afece8a1aa73c9227316a93b
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-11-22 23:17:06 +0100

    Simplify extensions

--- src/main/kotlin/org/danilopianini/gradle/latex/ProjectExtensions.kt
+++ src/main/kotlin/org/danilopianini/gradle/latex/ProjectExtensions.kt
@@ -1,10 +1,10 @@
 package org.danilopianini.gradle.latex
 
 import org.gradle.api.Project
-import org.gradle.api.provider.Property
+import org.gradle.kotlin.dsl.property
 
-inline fun <reified T> Project.propertyWithDefault(default: T): Property<T> =
-    objects.property(T::class.java).apply { convention(default) }
+inline fun <reified T> Project.propertyWithDefault(default: T) =
+    objects.property<T>().apply { convention(default) }
 
-inline fun <reified T> Project.propertyWithDefault(noinline default: () -> T): Property<T> =
-    objects.property(T::class.java).apply { convention(default()) }
+inline fun <reified T> Project.propertyWithDefault(noinline default: () -> T) =
+    objects.property<T>().apply { convention(default()) }
