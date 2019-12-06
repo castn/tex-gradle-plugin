3608d545eb7f1653cc77019b32072864177d95a3
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-12-06 20:03:52 +0100

    Apply lifecycle base plugin

--- src/main/kotlin/dev/reimer/tex/gradle/plugin/TexPlugin.kt
+++ src/main/kotlin/dev/reimer/tex/gradle/plugin/TexPlugin.kt
@@ -3,6 +3,7 @@ package dev.reimer.tex.gradle.plugin
 import dev.reimer.tex.gradle.plugin.task.TexCompile
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.language.base.plugins.LifecycleBasePlugin
 
 class TexPlugin : Plugin<Project> {
@@ -12,11 +13,13 @@ class TexPlugin : Plugin<Project> {
         const val EXTENSION_NAME = "tex"
         @Suppress("UnstableApiUsage")
         const val ASSEMBLE_TASK_NAME = LifecycleBasePlugin.ASSEMBLE_TASK_NAME
-        const val ASSEMBLE_TASK_PREFIX = "assemble"
-        const val ASSEMBLE_TASK_SUFFIX = "tex"
+        const val ASSEMBLE_TEX_TASK_NAME = ASSEMBLE_TASK_NAME + "Tex"
     }
 
     override fun apply(project: Project) {
+        @Suppress("UnstableApiUsage")
+        project.plugins.apply(LifecycleBasePlugin::class)
+
         // Register project extension.
         project.extensions.create(
             TexScope::class.java,
@@ -28,9 +31,7 @@ class TexPlugin : Plugin<Project> {
         val assemble = project.tasks.named(ASSEMBLE_TASK_NAME)
 
         // Register standard task for compiling TeX source set.
-        val assembleTex = project.tasks.register(
-            ASSEMBLE_TASK_PREFIX + ASSEMBLE_TASK_SUFFIX.capitalize()
-        ) { task ->
+        val assembleTex = project.tasks.register(ASSEMBLE_TEX_TASK_NAME) { task ->
             // Make TeX build a dependency of the global build task.
             assemble.get().dependsOn(task)
         }
