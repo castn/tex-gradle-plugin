ac05aded2d229e2e777e8188cb146b5a813b8281
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-12-06 21:20:28 +0100

    Fix typo

--- src/main/kotlin/dev/reimer/tex/gradle/plugin/internal/FileExtensions.kt
+++ src/main/kotlin/dev/reimer/tex/gradle/plugin/internal/FileExtensions.kt
@@ -4,7 +4,7 @@ internal object FileExtensions {
     const val AUX = "aux"
     const val BIB = "bib"
     const val BST = "bst"
-    const val BCF = "bcd"
+    const val BCF = "bcf"
     const val BBL = "bbl"
     const val PDF = "pdf"
     const val TEX = "tex"
