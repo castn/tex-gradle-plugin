7801b336cbdea0afe7eb16951980ee69c0a47d19
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-12-06 20:04:14 +0100

    Add file extensions

--- src/main/kotlin/dev/reimer/tex/gradle/plugin/internal/FileExtensions.kt
+++ src/main/kotlin/dev/reimer/tex/gradle/plugin/internal/FileExtensions.kt
@@ -4,6 +4,8 @@ internal object FileExtensions {
     const val AUX = "aux"
     const val BIB = "bib"
     const val BST = "bst"
+    const val BCF = "bcd"
+    const val BBL = "bbl"
     const val PDF = "pdf"
     const val TEX = "tex"
 }
