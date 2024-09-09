d15f1108db4a716ae72c4543db7e0de6cb1a3fee
castn <93142156+castn@users.noreply.github.com>
2024-09-09 20:21:36 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,13 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.22"
     `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "1.2.1"
-    id("org.jetbrains.dokka") version "0.10.0"
+    id("com.gradle.plugin-publish") version "1.2.2"
+    id("org.jetbrains.dokka") version "1.9.20"
     `maven-publish`
-    id("com.palantir.git-version") version "3.0.0"
+    id("com.palantir.git-version") version "3.1.0"
 }
 
 val gitVersion: groovy.lang.Closure<String> by extra
@@ -23,7 +21,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(gradleKotlinDsl())
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
 }
 
 lateinit var javadocJar: TaskProvider<Jar>
@@ -50,16 +48,11 @@ tasks {
         }
     }
 
-    // Generate Kotlin/Java documentation from sources.
-    dokka {
-        outputFormat = "html"
-    }
-
     // JAR containing Kotlin/Java documentation.
     javadocJar = register<Jar>("javadocJar") {
         group = JavaBasePlugin.DOCUMENTATION_GROUP
-        dependsOn(dokka)
-        from(dokka)
+        dependsOn(dokkaHtml)
+        from(dokkaHtml)
         archiveClassifier.set("javadoc")
     }
 
